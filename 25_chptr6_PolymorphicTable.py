54b08972a426f8d400e31e5113abe9fc7e27b03c
K4KarolE <karoly.egyed@protonmail.com>
2022-11-03 11:33:45 +0000

    closer / update

--- 25_chptr6_PolymorphicTable.py
+++ 25_chptr6_PolymorphicTable.py
@@ -1,10 +1,10 @@
 
 '''
 25 - Polymorphic Table - Chapter 6 - Practice
-- ask for the number of words should be displayed (1-100)
-- ask for the number of columns
-- determine how many items will be in 1/each column
-- determine the longest word in every column, that will be the width of that column
+- ask the user for the number of words should be displayed (1-100)
+- ask the user for the number of columns should we use
+- create a list for every column
+- find the longest word/item in every column, it`s length will be the width of that column
 '''
 
 
@@ -13,27 +13,23 @@
 
 table = ['zoo','fishy','coffee','fragile','mobile','cemetery','midget','toitoi','hangover']   # 9
 
-columnNumber = 3
+columnNumber = 1
+
 tableLength = len(table)
 
 averageColumnLength = int(tableLength / columnNumber)
 mod = tableLength % columnNumber           # if the tableLength/columnNumber not integer
                                            # we would add an extra item to the lists, starting with the 1st list
-                                           # example: 24 items in the chosen list, in 5 columns: we should place 1 extra item in the first 4 column
-
-print()
-if columnNumber >= tableLength:
-    for i in table:
-        print(i, end=' ')
-print('\n')
-
+                                           # example: 24 items in the chosen list, in 5 columns: we should place 1 extra item in the first 4 columns
 print()
 
 
 
+# NUMBER OF COLUMNS < NUMBER OF ITEMS
 # CREATING LISTS
 tableList = []
-columnWidth = []                  
+columnWidth = []
+tabletWidth = 0               
 for i in range(columnNumber):       
         tableList.append([])        # creating list for every column
         columnWidth.append([0])      # creating list for every column`s width
@@ -47,42 +43,44 @@ if columnNumber < tableLength:
             del table[averageColumnLength*i+p+1]    # removing this extra item from the original/chosen list, so the next list will not include this item
             mod -= 1
 
+
 # FINDING THE WIDTH OF EVERY COLUMN   
     for i in range(columnNumber):
         for p in range(len(tableList[i])):
             if len(tableList[i][p]) > columnWidth[i][0]:
                 columnWidth[i][0] = len(tableList[i][p])
+        tabletWidth = tabletWidth + columnWidth[i][0] + 2      # we will use this value to print the table title banner
 
-# PRINTING THE COLUMNS / TABLE
-    for p in range(len(tableList[p])):
-        for i in range(columnNumber):
-            print(tableList[i][p].rjust(columnWidth[i][0]), end = '   ')
+# PRINTING
+# COLUMNS NUMBER = 1
+if columnNumber == 1:
+    print('- YOUR TABLE -'.center(columnWidth[i][0],'-'))
+    tableList = tableList[0]
+    for item in tableList:
+        print(item.rjust(columnWidth[i][0]))
+
+# COLUMNS NUMBER > 1
+maxListLength = len(tableList[0])   # because we moved one of the "modulo" items into the 1st list (if there was any, like: 21 items in 5 columns)
+                                    # this list`s` length will be the maximum(or equal to other +modulo items list`s length, like: 24 items in 5 columns)
+if columnNumber > 1 and columnNumber < tableLength:
+    print('- YOUR TABLE -'.center(tabletWidth,'-'))
+    for g in range(maxListLength):
+        for k in range(columnNumber):
+            if g in range(len(tableList[k])):   # making sure there is no "out of index error" like: 21items/5columns -> 1st list`s length:5 / 2nd list`s length:4
+                print(tableList[k][g].rjust(columnWidth[k][0]), end = '  ')
+        
         print()
 
-                
-    
+
+# NUMBER OF COLUMNS > NUMBER OF ITEMS
+if columnNumber >= tableLength:
+    for i in range(len(table)):
+        tabletWidth = tabletWidth + len(table[i]) + 1
+    print('- YOUR TABLE -'.center(tabletWidth,'-'))
+    for i in table:
+        print(i, end=' ')
+    print()
 
 
-
-
-
-
-
-
-# print(columnWidth)
-# print(tableList)
 print()
-
-# FINDING THE LONGEST ITEM IN THE LISTS/COLUMNS
-
-    
-
-
-
-
-
-
-# print(averageColumnLength)
-# print(tableLength)
-# print(mod)
 
--- README.md
+++ README.md
@@ -24,7 +24,7 @@
 - the movie quote belongs to the keyword will be displayed and copied to the clipboard
 
 25 - Polymorphic Table - Chapter 6 - Practice
-- ask for the number of words should be displayed (1-100)
-- ask for the number of columns
-- determine how many items will be in 1/each column
-- determine the longest word in every column, that will be the width of that column
+- ask the user for the number of words should be displayed (1-100)
+- ask the user for the number of columns should we use
+- create a list for every column
+- find the longest word/item in every column, it`s length will be the width of that column
