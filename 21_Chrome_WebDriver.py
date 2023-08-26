6530f4840ba9c84c7b74b5c08865238f4426c94c
karoly.egyed@protonmail.com <karoly.egyed@protonmail.com>
2023-08-26 04:30:30 +0100

    remove sheet

--- 21_Chrome_WebDriver.py
+++ 21_Chrome_WebDriver.py
@@ -1,8 +1,9 @@
-
+import time
 from selenium import webdriver
 
 PATH = 'C:\Program Files (x86)\chromedriver.exe'
 driver = webdriver.Chrome(PATH)
 # driver.minimize_window()
 driver.get('https://www.imdb.com/title/tt0119822/')
+time.sleep(500)
                                            
